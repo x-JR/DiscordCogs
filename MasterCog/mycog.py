4a9492ea67699432b3145eb0ca030348d423ecf2
x-JR <80025021+x-JR@users.noreply.github.com>
2022-11-14 17:13:55 +1100

    Add files via upload

---
+++ MasterCog/mycog.py
@@ -0,0 +1,15 @@
+import discord
+from redbot.core import commands
+import random
+
+class MyCog(commands.Cog):
+    """My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def test(self, ctx):
+        """This does stuff!"""
+        # Your code will go here
+        await ctx.send("I can do stuff!")
