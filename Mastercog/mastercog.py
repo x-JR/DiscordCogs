c5dc08cd3c943ee4399a58cb62ceb50cff472258
Jake Ringstad <jake@tekkie.com.au>
2022-11-17 16:05:13 +1100

    Updated Commands

--- Mastercog/mastercog.py
+++ Mastercog/mastercog.py
@@ -22,21 +22,20 @@ class MasterCog(commands.Cog):
     @commands.command()
     async def noshow(self, ctx, user):
         """Calls out people who say they will be on at a certain time"""
-        current_time = time.strftime("%I:%M")
-        await ctx.send(f"{user} it's {current_time} and we're all still waiting...")
-
-    @commands.command()
-    async def Test2(self, ctx, arg1):
-        """Test Command"""
         msg = random.choice(await self.config.NoShowResponses())
         current_time=time.strftime("%I:%M")
         if "{user}" in msg and "{current_time}" in msg:
-            await ctx.send(msg.format(user=arg1, current_time=current_time))
+            await ctx.send(msg.format(user=user, current_time=current_time))
         else:
             await ctx.send(msg)           
 
     @commands.command()
-    async def Check(self, ctx,):
-        """Test Command"""
-        msg = await self.config.NoShowResponses()
-        await ctx.send(f"{msg}")                
+    async def noshow_add(self, ctx, response: str = None):
+        """Adds response to list"""
+        async with self.config.NoShowResponses() as responses:
+            if response in responses:
+                responses.remove(response)
+                await ctx.send("Response removed")
+            else:
+                responses.append(response)
+                await ctx.send("Response added")              
