d69339d1955139c20c6e8a7d7515cebe568fe1df
Jake Ringstad <jake@tekkie.com.au>
2022-11-18 19:04:47 +1100

    test

--- Mastercog/mastercog.py
+++ Mastercog/mastercog.py
@@ -59,6 +59,12 @@ class MasterCog(commands.Cog):
         channel = message.channel
         if message.author == self.bot.user:
             return        
-        if "cheese" in discord.message or "wallace" in discord.message or "gromit" in discord.message:
+        if "cheese" in discord.message.lower():
             response = random.choice(await self.config.WallaceResponses())
             await channel.send(response)
+        if "wallace" in discord.message.lower():
+            response = random.choice(await self.config.WallaceResponses())
+            await channel.send(response)      
+        if "gromit" in discord.message.lower():
+            response = random.choice(await self.config.WallaceResponses())
+            await channel.send(response)                     
